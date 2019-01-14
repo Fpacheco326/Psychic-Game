94d90842f30cb62a2766ba97c1bec594c8018f5f
Fernando Pacheco <fpacheco326@gmail.com>
2019-01-13 23:39:11 -0800

    finalized

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,7 @@ var guessesRemaining = 10;
 
 var compLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-
+// This randomizes the letter selection process that the computer chooses //
 function resetGame() {
     compLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 }
@@ -19,9 +19,9 @@ function resetGame() {
 
 
 document.onkeyup = function(event) {
-
+// This command is what applies the variable userGuess to the keys that are pressed on the keyboard//
     var userGuess = event.key;
-
+// this if function is what states that if the userGuess is equal to the comps chosen letter then the statement is true and therefor applies a win to the win container category and also resets the guesses remaining counter to 10 as well as empties the guesses so far array. Its also resets the game to a new letter of the computers choosing//
     if (userGuess === compLetter){
             wins++;
             guessesRemaining = 10;
@@ -31,6 +31,8 @@ document.onkeyup = function(event) {
  console.log(compLetter);          
 
     } else if (guessesRemaining - 1 === 0){
+// this function is what states that if and only if the guesses remaining depletes to 0 then the losses counter goes up by 1 and the guesses so far array empties and the game resets and another random letter is chosen by the computer//
+
             losses++;
             guessesRemaining = 10;
             guessesSoFar=[];
@@ -40,6 +42,7 @@ document.onkeyup = function(event) {
     
 
     } else {
+// this function is what states that if the user guesses the wrong letter then the guesses counter goes down by 1 and populates the guesses so far array with the incorrect letters that the user has guessed//        
         guessesRemaining--;
         guessesSoFar.push(userGuess);
         fillLettersGuessed = document.querySelector('#guessesSoFar').innerHTML = guessesSoFar.join('');
@@ -47,7 +50,7 @@ console.log(guessesSoFar);
     }
 
    
-
+// these three statements are what populates the wins, losses, and guesses remaining counters.//
          fillWins = document.querySelector('#wins').innerHTML = wins;
     
          fillLosses = document.querySelector('#losses').innerHTML = losses;
